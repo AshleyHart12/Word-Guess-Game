a0ac2a6e50e6da07c81e86fabd201e6f30ba5e97
Ashinator <Ahart425@gmail.com>
2018-12-04 21:29:45 -0700

    last

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,119 @@
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 // Selectable Word List
-var selectableWords = 
+ var selectableWords = 
 [
-    "gunfight"
-    "whiskey"
-    "saloon"
-    "cattle"
-    "stampede"
-    "grit"
-    "buzzards"
-    "chaps"
-    "horse"
-]
+    "gunfight",
+    "whiskey",
+    "saloon",
+    "cattle",
+    "stampede",
+    "grit",
+    "buzzards",
+    "chaps",
+    "horse",
+];
+ // Define Variables
+var gameStarted = false;
+var currentWord;
+var wordAsDashes;
+var guessesLeft;
+var lettersGuessed;
+var numWins = 0;
+var getNewWord;
+var wordPlace; 
+var correctGuesses;
+var wordAsArr = [];
+var dashesArray = [];
 
-const maxTries = 10; // maximum guesses allowed
-
-var guessedLetters = []; // Stores the letters guessed
-var currentWordIndex;    // Index of current word in the array
-var guessingWord = [];   // Word to build to match current word
-var remainingGuesses = 0; // How many tried user has left
-var gameStarted = false;  // Flag to tell if game has started
-var hasFinished = false;  // 'Press any key to try again'
-var wins = 0;             // How many wins the user has acquired
-
-//Set up all Variables for the start of the game
-function resetGame() { //reset game-level variables
-    remainingGuesses = maxTries;
-    gameStarted = false;
-
-    //Math.floor to round DOWN to the nearest whole
-    currentWordIndex = Math.floor(Math.random() * (selectableWords.length));
-    // Clear 
-    guessedLetters = [];
-    guessingWord = [];
-
-    // Build guessing word and clear it out
-    for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
-        guessingWord.push("_");
-    }
-    updateDisplay();
+// Start the game
+function initialize() {
+	gameStarted = true;
+	lettersGuessed = [];
+	correctGuesses = 0;
+	wordPlace = Math.floor(Math.random() * 9);
+	currentWord = selectableWords[wordPlace];			
+	guessesLeft = 9 - currentWord.length;		
+	wordAsDashes = makeIntoDashes(currentWord);	
+	wordAsArr = currentWord.split('');			
+	dashesArray = wordAsDashes.split('');		
+	document.getElementById("currentWord").innerHTML = wordAsDashes;
+	document.getElementById("lettersGuessed").innerHTML = "--";
+	document.getElementById("guessesLeft").innerHTML = guessesLeft;
 }
 
+// Make each word into underscores
+function makeIntoDashes(word) {
+	var dashes = "";
+	for (i = 0; i < word.length - 1; i++) {
+		dashes += "_ ";
+	}
+	dashes += "_";
+	return dashes;
+}
 
+// When the user strikes a key
+function playGame(letter) {
+	var letter = letter.toLowerCase();
+
+	// Make sure user enter a letter
+	if (alphabet.indexOf(letter) > -1) {
+		if (wordAsArr.indexOf(letter) > -1) {
+			correctGuesses++;
+			displayLetter(letter);
+		}
+		else {
+			if (lettersGuessed.indexOf(letter) > -1) {
+				return;
+			}
+			else {
+				guessesLeft--;
+				document.getElementById("guessesLeft").innerHTML = guessesLeft;
+				lettersGuessed.push(letter);
+				document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(' ');
+				if (guessesLeft == 0) {
+					alert("Sorry, fella! The correct answer is " + currentWord);
+					initialize();
+					numLosses++;
+					document.getElementById("losses").innerHTML = numLosses;
+				}
+			}
+		}
+	}
+}
+
+// Displays letter if it's in word
+function displayLetter(letter) {
+	// for each char in wordAsDashes, if matches currentWord --> display
+	for (i = 0; i < currentWord.length; i++) {
+		if (letter == wordAsArr[i]) {
+			dashesArray[i * 2] = letter;
+			console.log(dashesArray);
+		}
+	}
+	document.getElementById("currentWord").innerHTML = dashesArray.join("");
+	checkForWin();
+}
+
+// Checks for win by looking for "_"
+function checkForWin() {
+	if (dashesArray.indexOf("_") === -1) {
+		alert("Yeehaw Cowboy, Great Job! The correct answer is " + currentWord);
+		numWins++;
+		document.getElementById("wins").innerHTML = numWins;
+		initialize();
+	}
+}
+
+document.onkeyup = function (event) {
+	if (!gameStarted) {
+		initialize();
+		document.getElementById("currentWord").innerHTML = wordAsDashes.split(",");
+		console.log(currentWord);
+		gameStarted = true;
+	}
+	else {
+		playGame(event.key);
+	}
+}
